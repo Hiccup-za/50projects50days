c9b867615381e72e2702009ac76041a8873fe160
Christopher Zeuch <chriszeuch.cz@gmail.com>
2024-05-15 21:43:35 +0200

    PR updates

--- expanding_cards/script.js
+++ expanding_cards/script.js
@@ -4,11 +4,11 @@ panels.forEach(panel => {
   panel.addEventListener('click', () => {
     removeActiveClasses();
     panel.classList.add('active');
-  })
+  });
 });
 
 function removeActiveClasses() {
   panels.forEach(panel => {
     panel.classList.remove('active');
-  })
+  });
 }
